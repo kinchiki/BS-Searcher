cd8be9a0df0a17ee50d428944203c90b00b7740f
AkitoMizuno <mznakt@gmail.com>
2016-10-24 05:00:11 +0900

    企業情報のメイン業種以外のスクレイピングプログラム作成

--- scraping/company-scraping.rb
+++ scraping/company-scraping.rb
@@ -3,10 +3,34 @@ require 'open-uri'
 require 'nokogiri'
 require '../class/Company.rb'
 
-url = 'https://job.rikunabi.com/2017/company/top/r716010091/'
+url = 'https://job.rikunabi.com/2017/company/top/r484700037/'
 doc = Nokogiri::HTML.parse(open(url))
 
 company = Company.new
 
-company.name = "hoge"
+company.name = doc.xpath("//h1[@class='company-title-main']").text
 p company.name
+
+#メイン業種のリストを予め作っておき、そっからマッチしたものを取り出すとか
+#classを書き換えて抽出する？
+company.main_type = doc.xpath("//td[@class='company-information-detail']").first.text
+p company.main_type
+
+company.sub_type = doc.xpath("//span[@class='u-db u-fs14']").text
+p company.sub_type
+
+c = doc.xpath("//th[@class='company-data-th']")
+e_index = 0
+c.each_with_index { |node, i|
+  if node.text == "従業員数"
+    e_index = i
+    break
+  end
+}
+
+company.employees_number = doc.xpath("//td[@class='company-data-td']")[e_index].text.sub(/名.+/,"").sub(/[^\d]+/, "").delete(",").to_i
+#company.employees_number = doc.xpath("/html/body/div[1]/div[2]/div/div[8]/table/tbody/tr[4]/td")
+p company.employees_number
+
+company.head_office = doc.xpath("//td[@class='company-information-detail']")[1].text
+p company.head_office
--- scraping/seminar-url-extraction.rb
+++ scraping/seminar-url-extraction.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'kconv'
 
 1.upto(40) do |n|
-  url = "https://job.rikunabi.com/2017/search/seminar/result/?ms=1&isc=ps055&pn=" + n.to_s
+  url = "https://job.rikunabi.com/2017/search/seminar/result/?pn=" + n.to_s
   doc = Nokogiri::HTML(open(url))
 
   nodesets = doc.xpath("//div[@class='search-cassette-title']/a")
